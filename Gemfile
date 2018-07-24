033e2d1a88fddd7da25ed3aeee7d7fd2fe4c84af
kedarnag13 <kedarnag138@gmail.com>
2018-07-24 19:58:34 +0800

    [master] :sparkles: Adds pry to the application.

--- Gemfile
+++ Gemfile
@@ -35,8 +35,8 @@ gem 'rails-api'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  # Call 'pry' anywhere in the code to stop execution and get a debugger console
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
 end
